81dd5d3a03598d9e73eaf57644711006ca6d4da0
WillCui- <wyfcui@gmail.com>
2020-11-17 00:53:14 -0500

    animating the cube

--- assignment8/render.js
+++ assignment8/render.js
@@ -16,6 +16,12 @@ const drawScene = (gl, parameters, buffers, deltaTime) => {
         squareRotation,
         [0, 0, 1]);
 
+    glMatrix.mat4.rotate(
+        modelViewMatrix,
+        modelViewMatrix,
+        squareRotation * .7,
+        [0, 1, 0]);
+
     squareRotation += deltaTime;
 
     configurePositionBufferRead(gl, buffers, parameters);
@@ -23,9 +29,8 @@ const drawScene = (gl, parameters, buffers, deltaTime) => {
     
     gl.useProgram(parameters.program);
     setUniforms(gl, parameters, projectionMatrix, modelViewMatrix);
-    gl.drawArrays(gl.TRIANGLE_STRIP,
-        0, // offset
-        4); // vertexCount
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+    gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
 }
 
 const clearScene = (gl) => {
@@ -52,7 +57,7 @@ const createProjectionMatrix = (gl) => {
 
 const configurePositionBufferRead = (gl, buffers, parameters) => {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-    gl.vertexAttribPointer(parameters.attribLocations.vertexPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(parameters.attribLocations.vertexPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(parameters.attribLocations.vertexPosition);
 }
 
